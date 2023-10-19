6a40a7a4f2d755a6c161b4571c1413fc6394f076
Jensen Kokou <104699414+jensenkokou@users.noreply.github.com>
2023-10-19 16:10:49 -0700

    Update ec2-start-stop.sh

--- ec2-start-stop.sh
+++ ec2-start-stop.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Make sure to name your script ec2-start-stop.sh :) 
+# Make sure to name your script start-stop-ec2.sh :) 
 
 # Replace these values with your own AWS region and instance IDs
-AWS_REGION="us-east-1"
-INSTANCE_IDS="i-1234567890abcdef0 i-0987654321fedcba0" # You can list multiple instances or just one. 
+AWS_REGION="us-west-2" # Please make sure you're using the right region for your EC2 Instance
+INSTANCE_IDS="i-02878a844a070ed15" # You can add multiple instance IDS here 
 
 start_instances() {
     echo "Starting EC2 instances..."
     aws ec2 start-instances --instance-ids $INSTANCE_IDS --region $AWS_REGION
+
+    # Retrieve and display public IP addresses 
+    public_ips=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query 'Reservations[].Instances[].PublicIpAddress' --output text --region $AWS_REGION)
+    echo "Public IP addresses: $public_ips"
 }
 
 stop_instances() {
     echo "Stopping EC2 instances..."
     aws ec2 stop-instances --instance-ids $INSTANCE_IDS --region $AWS_REGION
+
+    # Retrieve and display public IP addresses
+    public_ips=$(aws ec2 describe-instances --instance-ids $INSTANCE_IDS --query 'Reservations[].Instances[].PublicIpAddress' --output text --region $AWS_REGION)
+    echo "Public IP addresses: $public_ips"
 }
 
 case "$1" in
@@ -30,3 +38,4 @@ case "$1" in
 esac
 
 exit 0
+
